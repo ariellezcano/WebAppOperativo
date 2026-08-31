9da0f7466df677b3ac36c5f9790bab60f92a5d7b
ariellezcano <ariellezcano1995@gmail.com>
2026-08-31 11:09:49 -0300

    ...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,8 +13,6 @@ const routes: Routes = [
   { path: '**', redirectTo: 'login' },
 ];
 
-
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+
+  URL: 'https://policiadigitalchaco.com.ar/ApiOperativo/api/',
+  URLPersona: 'https://policiadigital.chaco.gob.ar:9090/api_sistemas/policia',
+  URLUnidades: 'https://policiadigital.chaco.gob.ar:9090/api_mesaEntrada/',  
+  URLRegBus: 'https://policiadigital.chaco.gob.ar:9090/api_registroUsuario/',
 };
